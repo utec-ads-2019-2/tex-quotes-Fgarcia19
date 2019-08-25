972ea402f38d92a22cbfcadd2ca7600fe4bfd42b
Fgarcia19 <fabrizio.garcia@utec.edu.pe>
2019-08-25 18:21:39 +0000

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
+
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+
+
+void change(vector<string>inputs){
+int aux=0;
+    for(auto it:inputs){
+    for(int i=0;i<it.length();i++){
+        if(it[i]=='\"'){
+            switch(aux%2){
+                case 0:cout<<"``";
+                    break;
+                case 1:cout<<"\'\'";
+            }
+            aux++;
+        }
+        else
+            cout<<it[i];
+    }
+}
+
+
+}
+
+int main() {
+    string input;
+    vector<string>inputs;
+   do{
+        getline(cin,input);
+        inputs.push_back(input);
+    } while(input!="");
+   change(inputs);
+
+
 }
